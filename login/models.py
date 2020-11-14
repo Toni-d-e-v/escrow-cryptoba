f893b4cd6a01c4f0dabfb234dc17ef54e01b4866
Volodymyr Yahello <vyahello@gmail.com>
2020-11-14 11:42:18 +0200

    Implement login functionality

---
+++ login/models.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from login import db
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), unique=False, nullable=False)
+
+    def __repr__(self) -> str:
+        return f'User {self.username}'
